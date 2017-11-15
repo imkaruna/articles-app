98b0948fce2f5764dbe18a94323a5cf34cbfa144
Karuna <imkaruna@gmail.com>
2017-11-15 12:06:46 -0500

    Created models Tag and Author

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,16 @@
 class Article < ApplicationRecord
+  belongs_to :author
+  has_many :article_tags
+  has_many :tags, through: :article_tags
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115165806) do
+ActiveRecord::Schema.define(version: 20171115170523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,20 @@ ActiveRecord::Schema.define(version: 20171115165806) do
     t.text "description"
     t.integer "author_id"
     t.string "tags"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
