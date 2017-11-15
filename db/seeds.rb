b61f4a3b26b4ee17349c0491d4410d6412ae30c2
Karuna <imkaruna@gmail.com>
2017-11-15 12:14:08 -0500

    Seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+datafile = Rails.root + 'db/import.csv'
+
+i=1
+
+CSV.foreach(datafile, headers: true) do |row|
+
+  Article.find_or_create_by({id: row[0]}) do |hr|
+    if hr.id.present?
+      hr.id = row[0]
+      hr.title =  row[1]
+      hr.description =  row[2]
+      hr.author = Author.where(email: "fakemail#{i}@mail.com").first_or_create(name: row[3], password_digest: "test")
+      hr.all_tags =  row[4]
+      hr.created_at =  row[5]
+      hr.updated_at =  row[6]
+      i+=1
+    end
+  end
+
+end
